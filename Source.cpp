708147f61ac31fa5a232846afa142017dd8cd7d3
Crowrock1 <45708329+Crowrock1@users.noreply.github.com>
2020-01-06 11:44:36 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,30 +4,44 @@
 
 
 int main(int argc, char* argv[]) {
-	int hodnoty[9], i;
-	bool x,y = false;
-
-	for (int i = 0; i <= 8; i++) {
+	int y = 0, pocHod;
+	int* hodnoty;
+	bool x;
+    
+    while (true){
+        printf("pocet hodnot: ");
+        scanf("%d", &pocHod);
+        if (pocHod >= 3){
+            break;
+        }
+    }
+    
+	for (int i = 0; i <= pocHod - 1; i++) {
 		scanf("%d", &hodnoty[i]);
 	}
 
-	for (int i = 1; i <= 7; i++) {
+	for (int i = 1; i <= pocHod - 2; i++) {
 		if (hodnoty[i - 1] < hodnoty[i] && hodnoty[i] < hodnoty[i + 1]) {
 			x = true;
 		}
-		else if (hodnoty[i - 1] == hodnoty[i] || hodnoty[i] == hodnoty[i + 1]){
-		    y=true;
-		    
-		    }    
+		else if (hodnoty[i - 1] == hodnoty[i] && hodnoty[i] == hodnoty[i + 1]) {
+		       y = 2;
+		}
+		else if ((hodnoty[i - 1] == hodnoty[i]) ^ (hodnoty[i] == hodnoty[i + 1])) {
+            y = 1;    
+		}    
 		else if (hodnoty[i - 1] > hodnoty[i] && hodnoty[i] > hodnoty[i + 1]) {
-		    x = false;
+	        x = false;
 		}
 	}
-	if (y == false)
+	if (y == 0)
         printf(x ? "rostouci" : "klesajici");
         
-    else if (y == true)
+    else if (y == 1)
         printf(x ? "neklesajici" : "nerostouci");
 	
+	else if (y == 2)
+	    printf("konstantni");
+	    
 	return 0;
 }
